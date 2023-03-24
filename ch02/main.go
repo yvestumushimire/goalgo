7a2ebf1bb7584f189f57267e4f7fcbb0e660059b
Tumushimire  Yves <yvestumushimire@gmail.com>
2023-03-24 20:17:52 +0200

    insertion sort implementation in go 🤌

---
+++ ch02/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	arr := []int{31, 41, 59, 26, 41, 58}
+	result := InsertionSort(arr)
+	elapsed := time.Since(start)
+	fmt.Printf("results: %v took: %v \n", result, elapsed)
+	result2 := InsertionSortReverse(arr)
+	fmt.Printf("result2: %v took: %v \n", result2, elapsed)
+}
+
+func InsertionSort(arr []int) []int {
+	for j, v := range arr {
+		i := j - 1
+		for i >= 0 && arr[i] > v {
+			arr[i+1] = arr[i]
+			i = i - 1
+		}
+		arr[i+1] = v
+	}
+
+	return arr
+}
+
+func InsertionSortReverse(arr []int) []int {
+	for j, v := range arr {
+		i := j - 1
+		for i >= 0 && arr[i] < v {
+			arr[i+1] = arr[i]
+			i = i - 1
+		}
+		arr[i+1] = v
+	}
+
+	return arr
+}
